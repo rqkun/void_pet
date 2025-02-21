0b6bc71f38ce5a62afa286eb0eb9aaaacfd305e0
nhkd <nhkd@netcompany.com>
2025-02-21 03:32:11 +0000

    Added confirm dialog for clear caches

--- components/custom.py
+++ components/custom.py
@@ -1,6 +1,7 @@
 import math
 import uuid
 import streamlit as st
+from components import dialogs
 from config.constants import AppIcons, AppPages, Warframe
 import streamlit_antd_components as sac
 
@@ -12,24 +13,25 @@ def sideNav(current_idx):
         logo (enum, optional): current page logo. Defaults to Warframe.AYA.value.
     """
     with st.sidebar:
-        
+        st.markdown("""<style>.st-emotion-cache-kgpedg {padding: calc(1.375rem) 1.5rem 0rem; margin:-1rem 0 -1rem 0;}</style>""",unsafe_allow_html=True)
         selection = sac.menu([
             sac.MenuItem('Home', icon='house-fill'),
             sac.MenuItem('Vendors', icon='box-fill', children=[
                 sac.MenuItem("Baro Ki'ter", icon='arrow-repeat'),
                 sac.MenuItem('Varzia', icon='droplet-fill'),
             ]),
-            sac.MenuItem('Market', icon='shop-window'),
+            sac.MenuItem('Market', icon='shop-window', children=[
+                sac.MenuItem('Orders', icon='cart3'),
+                sac.MenuItem('Rivens', icon='dice-6'),
+            ]),
             sac.MenuItem('News', icon='newspaper'),
-            sac.MenuItem('Riven', icon='dice-6'),
             sac.MenuItem('Github', icon='github',description="rqkun/void_pet", href='https://github.com/rqkun/void_pet/')
         ], size='xs',variant='left-bar', return_index=True, open_all=True, index=current_idx)
         
         sac.divider(label='Settings', icon='gear', align='center', color='gray')
         
-        if st.button("Clear caches",type="secondary",use_container_width=True, help="Force reload data.",icon=AppIcons.SYNC.value):
-            st.cache_data.clear()
-            st.rerun()
+        if st.button("Clear caches",type="primary",use_container_width=True, help="Force reload data.",icon=AppIcons.SYNC.value):
+            dialogs.clear_cache_dialog()
         
     if selection != current_idx:
         if selection == 0:
@@ -38,12 +40,12 @@ def sideNav(current_idx):
             st.switch_page(AppPages.BARO.value)
         if selection == 3:
             st.switch_page(AppPages.VARZIA.value)
-        if selection == 4:
-            st.switch_page(AppPages.MARKET.value)
         if selection == 5:
-            st.switch_page(AppPages.NEWS.value)
+            st.switch_page(AppPages.MARKET.value)
         if selection == 6:
             st.switch_page(AppPages.RIVENS.value)
+        if selection == 7:
+            st.switch_page(AppPages.NEWS.value)
 
 
 def paginations(items,num_of_row=2,items_per_row = 5):
@@ -77,39 +79,52 @@ def reject_url_param():
 
 
 def empty_result(item):
+    """ Render Empty results."""
     sac.result(label='Empty', description=f"""Currently there's no {item}""", status='empty')
 
 
 def baro_time_alert(message):
+    """ Render baro kit'er alert message"""
     sac.alert(label=f"{message}", banner=True,size='xs',variant='outline', color='#4682b4', icon=True, closable=True)
 
 
 def inject_style(filename):
+    """ Inject style from html file."""
     st.html(f"""components/htmls/{filename}.html""")
 
 def auction_style():
+    """ Auction page style. """
     inject_style("auction")
 
 
 def image_style():
+    """ Image style. """
     inject_style("image")
 
 
 def varzia_style():
+    """ Varzia page style. """
     inject_style("varzia")
 
 
 def market_style():
+    """ Market page style. """
     inject_style("market")
 
 
 def world_style():
+    """ Home page style. """
     inject_style("world")
 
 
 def card_style():
+    """ Varzia/Baro cards style. """
     inject_style("card")
 
+def app_style():
+    """ App style. """
+    inject_style("app")
 
 def set_divider():
+    """ Render a divider. """
     sac.divider(label='item', icon='diagram-2-fill', align='center', color='gray')
