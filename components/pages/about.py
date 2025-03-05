f999495ae6af81d704c29b1380316c258f895018
nhkd <nhkd@netcompany.com>
2025-03-05 10:35:50 +0000

    Test V6

--- components/pages/about.py
+++ components/pages/about.py
@@ -8,39 +8,15 @@ import logging
 from supabase import create_client, Client
 import streamlit as st
 
+from datasources import warframe_export
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-# Initialize Supabase client
-SUPABASE_URL = st.secrets["supabase"]["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["supabase"]["SUPABASE_KEY"]
-BUCKET_NAME = "json-files"
-def get_manifest():
-        """Fetch and decompress the manifest file."""
-        try:
-            logging.info("Fetching manifest file.")
-            request_ref = "https://origin.warframe.com/PublicExport/index_en.txt.lzma"
-            
-            response = requests.get(request_ref)
-            response.raise_for_status()
-
-            logging.info("Decompressing manifest file.")
-            buf = bytearray(response.content)
-            buf[5:13] = b'\xff\xff\xff\xff\xff\xff\xff\xff'
-            st.download_button("a",response.content,"index_en.txt.lzma")
-            decompressed_data = lzma.decompress(buf, format=lzma.FORMAT_ALONE)
-            
-            return decompressed_data.decode("utf-8", errors='ignore').split("\r\n")
-        except (requests.RequestException, lzma.LZMAError) as e:
-            logging.error(f"Error fetching or decompressing manifest: {e}")
-            raise ValueError(f"Error fetching or decompressing manifest: {e}")
-
 
 if st.button("Run"):
     logging.info("Starting JSON upload process.")
     try:
-        st.write(get_manifest())
+        warframe_export.process_manifest()
         logging.info("All files uploaded successfully.")
     except Exception as e:
         logging.error(f"Error during upload process: {e}")
--- datasources/warframe_export.py
+++ datasources/warframe_export.py
@@ -1,8 +1,13 @@
 from enum import Enum
 import json
 import logging
+import lzma
+import re
 import requests
 import streamlit as st
+import urllib
+
+from supabase import Client, create_client
 
 from utils.api_services import raise_detailed_error
 
@@ -84,3 +89,74 @@ def export_open(item:Enum):
     """
     with open(f"""static/exports/{item["path"]}""", "r", encoding="utf-8") as file:
         return json.load(file)[item["object_name"]]
+
+def get_index_file():
+    """Fetch and decompress the manifest file."""
+    try:
+        logging.info("Fetching manifest file.")
+        request_ref = "https://origin.warframe.com/PublicExport/index_en.txt.lzma"
+        
+        response = requests.get(request_ref)
+        response.raise_for_status()
+
+        logging.info("Decompressing manifest file.")
+        buf = bytearray(response.content)
+        buf[5:13] = b'\xff\xff\xff\xff\xff\xff\xff\xff'
+        st.download_button("a",response.content,"index_en.txt.lzma")
+        decompressed_data = lzma.decompress(buf, format=lzma.FORMAT_ALONE)
+        
+        return decompressed_data.decode("utf-8", errors='ignore').split("\r\n")
+    except (requests.RequestException, lzma.LZMAError) as e:
+        logging.error(f"Error fetching or decompressing manifest: {e}")
+        raise ValueError(f"Error fetching or decompressing manifest: {e}")
+def process_manifest():
+    """Fetch and upload JSON files to Supabase."""
+    SUPABASE_URL = st.secrets["supabase"]["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["supabase"]["SUPABASE_KEY"]
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+    BUCKET_NAME = "json-files"
+    logging.info("Processing manifest files.")
+    manifest_files = get_index_file()
+
+    for file in manifest_files:
+
+        try:
+            file = file.replace(" ", "")
+            logging.info(f"Processing file: {file}")
+            encoded_name = urllib.parse.quote(file, safe="")
+            url = f"http://content.warframe.com/PublicExport/Manifest/{encoded_name}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+
+            # Clean and parse JSON
+            cleaned_json = re.sub(r'[\x00-\x1f\x7f]', '', response.text)
+
+            # Parse the JSON data
+            data = json.loads(cleaned_json)
+            json_data = json.dumps(data, indent=4)
+            # Ensure proper filename
+            file_name = file.split(".json")[0] + ".json"
+
+                    # Parse the JSON data
+
+            if ".json" in file:
+                file = file.split(".json")[0] + ".json"
+                # Save the JSON data to a file
+                # output_path = f"./static/exports/{file}"
+                # with open(output_path, "w", encoding="utf-8") as output_file:
+                #     json.dump(data, output_file, indent=4)
+                # logging.info(f"File '{file_name}' uploaded successfully saved to local.")
+            
+
+            # Upload to Supabase
+            try:
+                supabase.storage.from_(BUCKET_NAME).upload(file_name, json_data.encode('utf-8'), {"content-type": "application/json","upsert":"true"})
+                logging.info(f"File '{file_name}' uploaded successfully to bucket '{BUCKET_NAME}'.")
+            except Exception as e:
+                logging.error(e.args)
+
+
+        except (requests.RequestException, json.JSONDecodeError) as e:
+            logging.error(f"Error processing {file}: {e}")
